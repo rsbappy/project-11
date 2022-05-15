64024e101832a1f33a15e33ca8e90b38cb95629c
bappy <bappykhan226@gmail.com>
2022-05-15 19:33:22 +0600

    updatet

---
+++ main.js
@@ -0,0 +1,36 @@
+// $(document).ready(function(){
+
+// alert("jQuery Loaded");
+
+// });
+
+// $(document).ready(function () {
+
+//     $("button").click(function () {
+
+//         $("h1").toggle(100, function () {
+
+//             alert("djkjfkdj");
+//         });
+//     });
+
+
+
+
+// });
+
+
+
+
+
+$(document).ready(function () {
+    // make to functions to add and remove the class span
+
+    function add() {
+        $('.copied').addClass('bounce-effect')
+    }
+
+    function remove() {
+        $('.copied').removeClass('bounce-effect')
+    }
+});
